fb7a0f0e6d083f71e2347abe5cce6719f2e38fd8
sukrubeyy <sukru.beyy@outlook.com>
2023-11-18 02:43:52 +0300

    Mongo CRUD Operation Complete

--- src/NebulaPlugin.Application/DatabaseManager.cs
+++ src/NebulaPlugin.Application/DatabaseManager.cs
@@ -2,14 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 namespace NebulaPlugin.Application
 {
     public abstract class DatabaseManager
     {
-        public abstract void Update(params object[] parameters);
-        public abstract void Create(params object[] parameters);
-        public abstract void Delete(params object[] parameters);
-        public abstract void Read(params object[] parameters);
+        public abstract void CreateDatabase(string dbName);
+        public abstract void CreateTable(string dbName, string tableName);
+        public abstract void CreateItem(string dbName, string tableName, BsonDocument doc);
+        public abstract void UpdateDatabase(string oldDbName, string newDbName);
+        public abstract void UpdateTable(string dbName, string oldTableName, string newTableName);
+        public abstract void UpdateItem(string dbName,string tableName,BsonDocument doc);
+        public abstract void ReadDatabases();
+        public abstract void ReadTables(string dbName);
+        public abstract void ReadItems(string dbName, string tableName);
+        public abstract void DeleteDatabase(string dbName);
+        public abstract void DeleteTable(string dbName,string tableName);
+        public abstract void DeleteItem(string dbName, string tableName, string id);
     }
 }
